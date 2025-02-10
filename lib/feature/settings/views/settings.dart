5d9d184a4d7de3e719a23786010bb31cd0548dc3
HoussaineBenDhieb <131673785+houssainebendhieb@users.noreply.github.com>
2025-02-10 13:47:22 +0100

    fix.

--- lib/feature/settings/views/settings.dart
+++ lib/feature/settings/views/settings.dart
@@ -1,7 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_docs_clone/core/utils/di/get_instance.dart';
 import 'package:google_docs_clone/feature/auth/data/cubit/user_cubit.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingScreen extends StatefulWidget {
   const SettingScreen({super.key});
@@ -11,13 +13,22 @@ class SettingScreen extends StatefulWidget {
 }
 
 class _SettingsState extends State<SettingScreen> {
+  final SharedPreferences sharedPreferences = getIt.get<SharedPreferences>();
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: ElevatedButton(
-            onPressed: () {
-              context.read<UserCubit>().SignOut();
-            },
-            child: const Text("Sign Out")));
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text("${sharedPreferences.get("email")}"),
+        Center(
+            child: ElevatedButton(
+                onPressed: () {
+                  
+                  context.read<UserCubit>().SignOut();
+                },
+                child: const Text("Sign Out"))),
+      ],
+    );
   }
 }
